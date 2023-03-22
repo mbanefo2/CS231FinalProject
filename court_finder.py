982ac14678df6789557a83bdc8af3c98323d4c24
mbanefo2 <bryan.mbanefo@gmail.com>
2023-03-21 20:20:54 -0700

    added initial files

---
+++ court_finder.py
@@ -0,0 +1,194 @@
+import cv2
+import os
+import numpy as np
+
+class CourtFinder(object):
+	def __init__(self, filepath, result_path, debug=True):
+		self.corners = []
+		self.found_corners = False
+		self.debug = debug
+		self.filepath = filepath
+		self.result_path = result_path
+        
+		if not os.path.isdir(self.result_path):
+			os.makedirs(self.result_path)
+            
+		self.original_img = cv2.imread(filepath)
+		self.height, self.width = self.original_img.shape[:2]
+    
+	def get_image_object(self):
+		return cv2.imread(self.filepath)
+
+	def dim_non_white_colors(self, img):
+		# Apply a binary threshold to obtain a mask of white pixels
+		_, mask = cv2.threshold(img, 220, 255, cv2.THRESH_BINARY)
+
+		# Invert the mask to obtain a mask of non-white pixels
+		mask = cv2.bitwise_not(mask)
+
+		# Apply the mask to the input image to dim all non-white pixels
+		dimmed = cv2.bitwise_and(img, img, mask=mask)
+
+		if self.debug:
+			cv2.imwrite(f'{self.result_path}/dimmed.png', dimmed)
+
+    
+	def convert_to_grey(self, img):
+		# img = self.get_image_object()
+		return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+	def gaussian_blur(self, img, kernel_size=5):
+		blur = cv2.GaussianBlur(img, (kernel_size,kernel_size), 0)
+		if self.debug:
+			cv2.imwrite(f'{self.result_path}/g_blur.png', blur)
+		return blur
+
+	def adaptive_threshold(self, img):
+		thresh = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 15, 2)
+        
+		if self.debug:
+			cv2.imwrite(f'{self.result_path}/thresh.png', thresh)
+		return thresh
+    
+	def morph_open(self, img, kernel_size=2):
+		kernel = np.ones((kernel_size,kernel_size), np.uint8)
+		opening = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+
+		if self.debug:
+			cv2.imwrite(f'{self.result_path}/morph_open.png', opening)
+		return opening
+
+	def canny_edge(self, img, thresh1=50, thresh2=100, aperture_size=5):
+		edges = cv2.Canny(img, thresh1, thresh2, apertureSize=aperture_size, L2gradient=True)
+
+		if self.debug:
+			cv2.imwrite(f'{self.result_path}/edges.png', edges)
+
+		return edges
+
+	def dilate_img(self, img, kernel_size=2):
+		kernel = np.ones((kernel_size, kernel_size), np.uint8)
+		dilated_edges = cv2.dilate(img, kernel, iterations=1)
+
+		if self.debug:
+			cv2.imwrite(f'{self.result_path}/dilated_edges.png', dilated_edges)
+		return dilated_edges
+
+	def find_contours(self, img):
+		contours, hierarchy = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+		rect_contours = []
+		for contour in contours:
+		# Approximate the contour to reduce the number of points
+			approx = cv2.approxPolyDP(contour, 0.01*cv2.arcLength(contour, True), True)
+		
+			# If the contour has four corners, it's likely the badminton court
+			if len(approx) == 4:
+				# Verify that the contour is roughly rectangular in shape
+				if cv2.isContourConvex(approx):
+					rect_contours.append(approx)
+					# new_img = cv2.drawContours(new_img, [approx], 0, (0, 0, 255), 2)
+
+		if not rect_contours:
+			raise ValueError('No rectangular contours found') 
+		return rect_contours
+
+	def find_largest_contour(self, contours):
+		areas = [cv2.contourArea(c) for c in contours]
+		max_index = np.argmax(areas)
+		largest_contour = contours[max_index]
+		approx = cv2.approxPolyDP(largest_contour, 0.01*cv2.arcLength(largest_contour, True), True)
+		return approx, largest_contour
+
+	def get_dominant_color(self, img):
+        # Initialize the result as a zero-filled NumPy array of shape (3,)
+		result = np.zeros((3,), dtype=int)
+
+    	# Define the number of bins and the bin width
+		bins = 64
+		bin_width = 256 // bins
+
+    	# Loop over each color channel
+		for i in range(3):
+        	# Compute the color histogram for the current channel
+			hist = cv2.calcHist([img], [i], None, [bins], [0, 256])
+
+        	# Smooth the histogram by adding neighboring bin values
+			hist_smoothed = np.convolve(hist.squeeze(), [1, 2, 1], mode='same')
+
+        	# Find the index of the peak in the smoothed histogram
+			peak_index = np.argmax(hist_smoothed)
+
+        	# Compute the value of the dominant color for the current channel
+			color_value = (peak_index + 0.5) * bin_width
+
+        	# Store the value in the result array
+			result[i] = color_value
+
+		# Return the result as a NumPy array of shape (3,)
+		return result
+
+	def get_rgb_mask(self, img):
+		# Find the biggest region that closely matches the court's average color in RGB space
+		win_rgb = img.copy()
+		win_dominant_rgb = self.get_dominant_color(win_rgb)
+
+		# Define the color thresholds for the RGB mask
+		color_thresh = 40
+		lower_rgb = win_dominant_rgb - color_thresh
+		upper_rgb = win_dominant_rgb + color_thresh
+
+		# Create the RGB mask
+		rgb_mask = cv2.inRange(img, lower_rgb, upper_rgb)
+		if self.debug:
+			cv2.imwrite(f'{self.result_path}/rgb_mask.png', rgb_mask)
+       
+		return rgb_mask
+
+	def get_hsv_mask(self, img):
+		# Find the biggest region that closely matches the court's average color in HSV space
+		win_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+		win_dominant_hsv = self.get_dominant_color(win_hsv)
+
+		# Define the color thresholds for the HSV mask
+		sat_thresh = 4
+		hue_thresh = 30
+		val_thresh = 1000
+		lower_hsv = win_dominant_hsv - [sat_thresh, hue_thresh, val_thresh]
+		upper_hsv = win_dominant_hsv + [sat_thresh, hue_thresh, val_thresh]
+
+		# Create the HSV mask
+		hsv_frame = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+		hsv_mask = cv2.inRange(hsv_frame, lower_hsv, upper_hsv)
+
+		# Optionally save the mask to disk
+		if self.debug:
+			cv2.imwrite(f'{self.result_path}/hsv_mask.png', hsv_mask)
+
+	def bitwise_mul(self, mask1, mask2):
+		new_mask = cv2.bitwise_and(mask1, mask2)
+
+		if self.debug:
+			cv2.imwrite(f'{self.result_path}/new_mask.png', new_mask)
+   
+	def contour_from_mask(self, mask, elliptical_shaper=20):
+		# Perform morphological closing to fill in small gaps in the court mask
+		kernel_size = int(self.width / elliptical_shaper)
+		kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (kernel_size, kernel_size))
+		closed_mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+
+		# Optionally save the closed mask to disk
+		if self.debug:
+			cv2.imwrite(f'{self.result_path}/closed_mask.png', closed_mask)
+
+		# Find the largest contour in the closed mask. This is assumed to be the court.
+		contours, hierarchy = cv2.findContours(closed_mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+		max_area = 0
+		max_contour = None
+		for contour in contours:
+			area = cv2.contourArea(contour)
+			if area > max_area:
+				max_area = area
+				max_contour = contour
+		return max_contour
+
+
---
+++ kalman_filter.py
@@ -0,0 +1,83 @@
+# from filterpy.kalman import KalmanFilter
+# from filterpy.common import Q_discrete_white_noise
+import numpy as np
+
+
+class KalmanFilter(object):
+    """
+    This code implements a Kalman filter to estimate the position and velocity of a shuttlecock in 3D space. 
+    The dynamics of the shuttlecock are assumed to be such that the x and z velocity of the shuttlecock should be constant,
+    while the y velocity is dependent on the acceleration due to gravity. 
+    The filter takes as input measurements of the shuttlecock's position,
+    and uses these to estimate the current state of the shuttlecock. 
+    The filter also uses a model of the dynamics of the shuttlecock to predict the state of the shuttlecock at the next time step, 
+    and updates its estimate based on the next measurement.
+
+    Args:
+        object (Class): Kalman filter
+    """
+    def __init__(self, framerate):
+        # shuttlecock parameters
+        self.accel_gravity = -9.8 # Acceleration due to gravity
+        self.shuttle_mass = 0.005 # Average weight of a badminton shuttlecock in kg
+        self.dt = 1.0 / framerate
+        
+        # Initialize state estimation parameters
+        self.mu_k = np.zeros((6, 1)) # Initial estimate of the mean of the shuttlecock state
+        self.sigma_k = np.eye(6) * 100.0 # Initial estimate of the covariance of the shuttlecock state
+        self.Q = np.identity(6) * self.dt # Model noise covariance
+        self.R = 0.05 * np.identity(3) # Measurement noise covariance, threshold for ray intersect distance
+
+        # Initialize the matrices for the shuttlecock dynamics and the measurement model
+        self.A_k = np.identity(6) + np.eye(6, k=3) * self.dt # Dynamics matrix
+        self.Bk_uk = np.array([[0], [0.5 * self.accel_gravity * self.dt * self.dt], [0], [0], [self.accel_gravity * self.dt], [0]]) # Control input matrix
+        self.C_k = np.hstack((np.identity(3), np.zeros((3, 3)))) # Measurement matrix
+    
+    def update(self, measured_pos):
+        """
+        Takes as input a measurement of the ball position
+        Update the estimate of the state based on the input measurement
+        
+        :param measured_pos (array): Current measured position of the shuttlecock
+        
+        :returns up_mu_k (array): New state estimate of the shuttlecock
+        :returns up_sigma_k (matrix): New Covariance matrix of the shuttlecock state
+        """
+        # Reshape the measurement vector
+        measured_pos = np.reshape(measured_pos, (3,1))
+        
+        # Compute covariances of the measurements
+        cov = self.C_k @ self.sigma_k @ self.C_k.T + self.R
+        cov_inv = np.linalg.inv(cov)
+        sigma_c = self.sigma_k @ self.C_k.T # Cross covariance btw state and measurement
+        
+        # Compute measurement error
+        meas_err = measured_pos - self.C_k @ self.mu_k
+        
+        # Update estimates and state covariances
+        up_mu_k = self.mu_k + sigma_c @ cov_inv @ meas_err
+        up_sigma_k = self.sigma_k - sigma_c @ cov_inv @ (self.C_k @ self.sigma_k)
+        
+        return up_mu_k, up_sigma_k
+    
+    def predict(self):
+        """
+        Predict the state of the shuttlecock at the next time step
+        based on current state estimates
+        
+        :returns hit_ground (bool): Flag to check if the shuttlecock has hit the ground or not
+        """
+        hit_ground = False
+        
+        # Update state covariance and state estimate
+        new_sigma_k = self.A_k @ self.sigma_k @ self.A_k.T + self.Q
+        new_mu_k = self.A_k @ self.mu_k + self.Bk_uk
+        
+        # Check if the position hits the ground, setting a small postive threshold value to account for noise
+        if new_mu_k[1] <= 0.05:
+            hit_ground = True
+        
+        self.mu_k = new_mu_k
+        self.sigma_k = new_sigma_k
+        
+        return hit_ground
